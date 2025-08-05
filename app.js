db6d15e4ff6295a767e88e159b5c1759b578461b
Rajdeep Pal <palrajdeep.2002@gmail.com>
2025-08-05 19:13:11 +0530

    Comment

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ app.use('/api/chatbot', chatbotRoutes);
 
 // Start server
 app.listen(port, () => {
-  console.log(`🚀 Server running at http://localhost:${port}`);
+  console.log(` Server running at http://localhost:${port}`);
 });
